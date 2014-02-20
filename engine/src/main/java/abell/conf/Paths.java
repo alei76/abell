973b170a3152f9fc5793f3517e61d452b8e17989
Guoyu <guoyu.511@gmail.com>
2014-02-21 01:02:34 +0800

    MOD mapreduce

--- engine/src/main/java/abell/conf/Paths.java
+++ engine/src/main/java/abell/conf/Paths.java
@@ -5,7 +5,9 @@ import org.apache.hadoop.fs.Path;
 public interface Paths {
 
 	Path ITEMS = new Path("/abell/items");
-    Path DICTIONARY = new Path("/abell/dictionary");
+    Path DICTIONARY = new Path("/abell/dict");
+    Path MATRIX = new Path("/abell/matrix");
+    Path MATRIX_ITEMS = new Path(MATRIX, "items");
 
 	Path TF_IDF = new Path("/abell/tfidf");
 	Path TF_IDF_STEP1 = new Path(TF_IDF, "step1");
---
+++ engine/src/main/java/abell/mapreduce/normalize/VectoringMapper.java
@@ -0,0 +1,43 @@
+package abell.mapreduce.normalize;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.commons.math.linear.SparseFieldVector;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.mahout.math.RandomAccessSparseVector;
+import org.apache.mahout.math.Vector;
+import org.apache.mahout.math.VectorWritable;
+
+import abell.conf.Paths;
+import abell.mapreduce.util.Dictionary;
+
+public class VectoringMapper extends Mapper<Text, Text, Text, VectorWritable> {
+
+	Dictionary dict;
+	
+	@Override
+	protected void setup(Context context)
+			throws IOException, InterruptedException {
+		dict = Dictionary.read(context.getConfiguration(), Paths.DICTIONARY);
+	}
+	
+	@Override
+	protected void map(Text key, Text value, Context context)
+			throws IOException, InterruptedException {
+		Vector vector = new RandomAccessSparseVector(dict.size());
+		StringTokenizer st = new StringTokenizer(value.toString());
+        while (st.hasMoreTokens()) {
+            String wordStr = st.nextToken();
+            if (wordStr.trim().length() == 0) {
+                continue;
+            }
+            int index = dict.indexOf(wordStr);
+            double count = vector.get(index);
+            vector.set(index, count + 1);
+        }
+        context.write(key, new VectorWritable(vector));
+	}
+
+}
